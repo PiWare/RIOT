a4590407ec59f9e364ca451efac3551c672eecaa
Thomas Bernard <thomas@famillebernargouriou.fr>
2016-07-05 00:00:32 +0200

    CMake build environment initial commit

---
+++ CMake/Platform/RIOT.cmake
@@ -0,0 +1,26 @@
+set_property(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS FALSE)
+
+set(CMAKE_STATIC_LIBRARY_PREFIX "")
+set(CMAKE_STATIC_LIBRARY_SUFFIX ".a")
+set(CMAKE_EXECUTABLE_SUFFIX "")
+set(CMAKE_C_OUTPUT_EXTENSION ".o")
+set(CMAKE_ASM_OUTPUT_EXTENSION ".o")
+set(CMAKE_CXX_OUTPUT_EXTENSION ".o")
+
+function(module name )
+    set(options )
+    set(oneValueArgs )
+    set(multiValueArgs SOURCES REQUIRES)
+    cmake_parse_arguments(module "${options}" "${oneValueArgs}"
+                          "${multiValueArgs}" ${ARGN} )
+
+                      add_library( ${name} STATIC ${module_SOURCES})
+    target_include_directories( ${name} PUBLIC include)
+    target_compile_definitions( ${name} PUBLIC "MODULE_$<UPPER_CASE:${name}>")
+    if(NOT "${name}" STREQUAL "cpu")
+        target_link_libraries( ${name} PRIVATE cpu)
+    endif()
+    if(NOT "${name}" STREQUAL "board")
+        target_link_libraries( ${name} PRIVATE board)
+    endif()
+endfunction()
---
+++ CMake/arm-gcc.toolchain.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME RIOT)
+
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE NEVER)
+
+set(CMAKE_CXX_COMPILER_WORKS TRUE)
+set(CMAKE_C_COMPILER_WORKS TRUE)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11 -Wstrict-prototypes -Werror=strict-prototypes -Wold-style-definition -Werror=old-style-definition -Wall")
+set(CMAKE_C_LINK_EXECUTABLE "${CMAKE_CXX_COMPILER} <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> -specs=nano.specs -Wl,-Map,<TARGET>.map -Wl,--start-group <OBJECTS> <LINK_LIBRARIES> -Wl,--end-group -o <TARGET>")
