3753be61f5a50b430768d39c33e973bf687ac473
Benjamin Füldner <benjamin@fueldner.net>
2016-07-05 15:47:12 +0200

    Clean up python script.

--- examples/housebus/tools/alternate_pins.py
+++ examples/housebus/tools/alternate_pins.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-import os
-import sys
 import csv
 import argparse
 import traceback
@@ -10,16 +8,7 @@ if __name__ == "__main__":
     try:
         parser = argparse.ArgumentParser(description = 'Alternate function template generator from csv table.')
         parser.add_argument('--csv', type = str, help = 'CSV formatted input table', required = True)
-    #   parser.add_argument('--symbol', type = str, help = 'Output file for generated KiCAD symbols', required = True)
-    #   parser.add_argument('--desc', type = str, help = 'Output file for generated KiCAD symbol description', required = True)
-    #   parser.add_argument('--template_path', type = str, help = 'Path to template symbols', required = True)
-    #   parser.add_argument('--table_path', type = str, help = 'Path to table based symbols', required = True)
         args = parser.parse_args()
-
-#       symbol_output = open(args.symbol, "w")
-#       symbol_output.write("EESchema-LIBRARY Version 2.3\n#encoding utf-8\n")
-#       desc_output = open(args.desc, "w")
-#       desc_output.write('EESchema-DOCLIB Version 2.0\n')
 
         with open(args.csv, 'rb') as csvfile:
             table = csv.reader(csvfile, delimiter=',', quotechar='\"')
